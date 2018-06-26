c2216558e7e2690d54b6c08c331c0a64cfd8db65
arturops <arturo.ps14@gmail.com>
2018-06-26 19:46:15 -0400

    Type: Add Form | Added a flask form to ask the user for their github username and the target repository to create and copy the web app code

---
+++ forms.py
@@ -0,0 +1,15 @@
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class GithubUserForm(FlaskForm):
+	github_username = StringField('Username', 
+									validators=[DataRequired(),Length(min=2, max=30)])
+
+	target_repo = StringField('Target Repo Name', validators=[DataRequired()])
+
+
+	submit = SubmitField('Get Code!')
