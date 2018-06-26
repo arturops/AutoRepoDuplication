d24a11dd091476334ba6eccf9a234566f7192a97
arturops <arturo.ps14@gmail.com>
2018-06-26 19:42:11 -0400

    Type: Update | Updated the main of the flask app to have home, about, access github user info, and error routings with their corresponding logic

--- repoapp.py
+++ repoapp.py
@@ -6,15 +6,52 @@
 # Linux, MacOSX: $ export FLASK_APP=AutoRepoDuplication.py
 # Windows: $ set FLASK_APP=AutoRepoDuplication.py
 
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import GithubUserForm
+import apiwrap
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+app.config['SECRET_KEY'] = '2aba1f6ebe92e925ec34c8486003cf08'
+
+
+@app.route('/')
+@app.route('/home')
 def home():
-	return "<h1>Hello Arturo!!</h1>"
+	return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+	version = {
+		'author' : 'Arturo Parrales Salinas',
+		'title' : 'Auto Repo Duplication App',
+		'ver' : '0.0.0.1',
+		'date' : 'June 25, 2018'
+	}
+	return render_template('about.html', title='About', version=version)
+
+
+@app.route('/access',methods=['GET','POST'])
+def access():
+	form = GithubUserForm()
+	if form.validate_on_submit():
+		apiwrap.run()
+		repo = 'https://github.com/{}/{}'.format(form.github_username.data, form.target_repo.data)
+		success_str = 'Repo {} has been created! Thanks {}!'.format(repo,form.github_username.data)
+		flash(success_str,'success')
+		flash(repo,'success')
+		return redirect(url_for('home'))
+	return render_template('access.html',title='User', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+
+
 
 
 @app.route('/list')
